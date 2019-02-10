635da2a63a4ce6f90eabe763cd3d36d409ed773b
Chris Ward <chriswhward@gmail.com>
2019-02-10 12:35:54 +0000

    Refactor to use NPM module

---
+++ test.js
@@ -0,0 +1,6 @@
+// TODO: Refactor correctly
+
+var testLexicon = require("./node_modules/odtweg/index.js");
+
+var testText = testLexicon.generateLexicon();
+console.log(testText);
--- twitter.js
+++ twitter.js
@@ -1,21 +1,18 @@
-let Twit = require('twit');
-require('dotenv').config();
+let Twit = require("twit");
+require("dotenv").config();
 
-var tweetLexicon = require('./createlexicon.js');
+var tweetLexicon = require("./node_modules/odtweg/index.js");
 
-var tweetText = tweetLexicon.randomString();
+var tweetText = tweetLexicon.generateLexicon();
 console.log(tweetText);
 
-const T = new Twit(
-    {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token: process.env.TWITTER_ACCESS_TOKEN,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    }
-);
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-
-T.post('statuses/update', { status: tweetText + " #boardgames" }, (err, data, response) => {
-    console.log(data)
+T.post("statuses/update", { status: tweetText }, (err, data, response) => {
+  console.log(data);
 });
